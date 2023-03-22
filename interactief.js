fff70e4f4da13e0c4b177c1dd8c96cada262c70d
Thiery <34451916+THM28@users.noreply.github.com>
2023-03-23 00:43:42 +0100

    Add files via upload

--- interactief.js
+++ interactief.js
@@ -16,3 +16,30 @@ window.addEventListener("click", function(event) {
     modal.style.display = "none";
   }
 });
+
+
+function openNewModal() {
+  // Get the new modal element by ID
+  const newModal = document.getElementById("newModal");
+  
+  // Get the close button element inside the new modal
+  const newModalCloseBtn = newModal.querySelector(".close");
+  
+  // When the user clicks the close button, hide the new modal
+  newModalCloseBtn.onclick = function() {
+      newModal.style.display = "none";
+  };
+  
+  // Show the new modal
+  newModal.style.display = "block";
+}
+
+hobbiesList.forEach((hobby, index) => {
+  hobby.addEventListener("click", () => {
+    hobbyTitle.innerText = hobby.innerText;
+    hobbyInfo.innerText = getHobbyInfo(index);
+    hobbiesPopup.style.display = "block";
+    // Show the modal
+    newModal.style.display = "block";
+  });
+});
